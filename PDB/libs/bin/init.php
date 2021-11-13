b1b9b6e55fc3a41ac37bd574a42fe82ed3c6ab03
average-insane-insomniac <94238636+average-insane-insomniac@users.noreply.github.com>
2021-11-13 12:38:04 -0500

    Update init.php

--- PDB/libs/bin/init.php
+++ PDB/libs/bin/init.php
@@ -4,7 +4,7 @@ require(dirname(__DIR__)."/handler/Exception.php");
 # package name
 const LIBRARY_NAME = "PDB";
 const LIBRARY_VERSION = "0.0.1";
-const LIBRARY_API = ["0.0.1"];
+const LIBRARY_API = "1";
 const LIBRARY_SSL_SUPPORT = true;
 const LIBRARY_LICENCE = "Apache-2.0 License";
 
