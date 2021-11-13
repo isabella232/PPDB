4f5bbe1f00ecc1a082cd40fed4ab0c476edfd001
surveybuilderadmin <64079569+surveybuilder-admin@users.noreply.github.com>
2021-11-13 06:54:15 -0500

    Create init.php

---
+++ PDB/libs/bin/init.php
@@ -0,0 +1,10 @@
+<?php
+# package name
+const LIBRARY_NAME = "PDB";
+const LIBRARY_VERSION = "0.0.1";
+const LIBRARY_API = ["0.0.1"];
+const LIBRARY_SSL_SUPPORT = true;
+const LIBRARY_LICENCE = "Apache-2.0 License";
+
+
+?>
