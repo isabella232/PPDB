8e483f0df44343e5a50756b3baf2845cb6f9082a
surveybuilderadmin <64079569+surveybuilder-admin@users.noreply.github.com>
2021-11-13 12:22:22 -0500

    Add files via upload

--- PDB/libs/bin/init.php
+++ PDB/libs/bin/init.php
@@ -1,10 +1,28 @@
 <?php
+require(dirname(__DIR__)."/handler/Exception.php");
+
 # package name
-const LIBRARY_NAME = "PDB";
+const LIBRARY_NAME = "";
 const LIBRARY_VERSION = "0.0.1";
 const LIBRARY_API = ["0.0.1"];
 const LIBRARY_SSL_SUPPORT = true;
 const LIBRARY_LICENCE = "Apache-2.0 License";
 
 
+
+
+
+# render
+   function RenderLibrary(){
+			# cache LIBRARY values
+		try{
+		if(LIBRARY_NAME !== "PDB"){
+			throw new PDBErr(LIBRARY_NAME);
+		}
+	}catch(PDBErr $e){
+			  echo $e->CHECKLIBSNAME();
+	}
+}
+
+RenderLibrary();
 ?>
