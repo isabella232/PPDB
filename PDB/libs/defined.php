8312e310cea0fede0969e265cf65924e6682c44b
surveybuilderadmin <64079569+surveybuilder-admin@users.noreply.github.com>
2021-11-13 21:56:06 -0500

    Add files via upload

---
+++ PDB/libs/defined.php
@@ -0,0 +1,6 @@
+<?php
+define("DS", "\\", false);
+define("ROOT", dirname(__FILE__), false);
+define("ROOT_DB", dirname(__FILE__).DS."db", false);
+
+?>
--- PDB/libs/pdb.lib.php
+++ PDB/libs/pdb.lib.php
@@ -1,12 +1,15 @@
 <?php
+require("defined.php");
 require("handler/Exception.php");
 require("bin/init.php");
 class PDB{
 	private function __construct(){
-		/*nothing/	
+	 #nothing	
+	}
+	public static function createStorage(){
+		
 	}
 
-	public static function createStorage();
 
 }
 
