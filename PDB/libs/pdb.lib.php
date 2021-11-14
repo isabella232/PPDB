6cb22820232eafc1e98e1182aeac1f74f34a19ac
surveybuilderadmin <64079569+surveybuilder-admin@users.noreply.github.com>
2021-11-14 15:11:57 -0500

    Add files via upload

--- PDB/libs/pdb.lib.php
+++ PDB/libs/pdb.lib.php
@@ -1,10 +1,35 @@
 <?php
-require("defined.php");
+require(dirname(__DIR__)."/defined.php");
 require("handler/Exception.php");
 require("bin/init.php");
 class PDB{
 	private function __construct(){
 	 #nothing	
+	}
+	public static function INSTALL($user, $psw, $host=PDB_CONNECT){
+		$pass = 1;
+		try{
+			if($host !== PDB_CONNECT){
+				throw new PDBErr($host);
+			}
+		}catch(PDBErr $e){
+			echo $e->CONNECT_ERR();
+			$pass = 0;
+		}
+		if($pass == 1){
+			$psw = hash("gost", $psw);
+			$psw = hash("sha1", $psw);
+			$psw = hash("md5", $psw);
+			$psw = hash("crc32b", $psw);
+			$psw = hash("ripemd128", $psw);
+			if(!file_exists(ROOT."user.json")){
+				
+				
+			}
+		}
+		
+		
+		
 	}
 	public static function createStorage(){
 		#Check if dictionary 
