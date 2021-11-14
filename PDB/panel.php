9c53a7bcfd0b02154d0812016c890e9bb0ef72ba
surveybuilderadmin <64079569+surveybuilder-admin@users.noreply.github.com>
2021-11-14 07:11:57 -0500

    Add files via upload

---
+++ PDB/panel.php
@@ -0,0 +1,15 @@
+<?php 
+require('./libs/pdb.lib.php');
+ require('./libs/pdb.sql.php');
+ ?>
+<html>
+<head>
+<title>Panel - <?php echo $_SERVER['HTTP_HOST'];?></title>
+</head>
+<body>
+<?php
+# Write code here
+
+?>
+</body>
+</html>
