58bd4d876a799ac3bde0739096dc238a50257e73
surveybuilderadmin <64079569+surveybuilder-admin@users.noreply.github.com>
2021-11-15 07:50:06 -0500

    Update pdb.sql.php

--- PDB/libs/pdb.sql.php
+++ PDB/libs/pdb.sql.php
@@ -1,5 +1,44 @@
 <?php
 class mySQL{
+	private $host;
+	private $user;
+	private $psw;
+	private $db;
+	public $conn;
 	
+	private function __contruct(){
+		#nothing
+	}
+	public static function connect($host, $user, $psw, $db){
+		$conn = new sqli($host, $user, $psw, $db);
+	if ($conn->connect_error) {
+  		die("Connection failed: " . $conn->connect_error);
+		}
+	}
+	public static function exportAll($table){
+	  $sql = "select * from '.$table.'";
+    	  $result = mysqli_query($conn, $sql) or die("Error in Selecting " . mysqli_error($conn));
+		$arr = array();
+		while($row = mysqli_fetch_assoc($result)){
+		  $result[] = $row;
+		}
+		$encode = json_encode($arr);
+		$export = fopen(ROOT_DB.DS."SQLALL.json", "w+");
+		fwrite($export, $encode);
+		fclose($export);
+	}
+       public static function export($table, $selector){
+       	$sql = "select '.$selector.' from '.$table.'";
+    	  $result = mysqli_query($conn, $sql) or die("Error in Selecting " . mysqli_error($conn));
+		$arr = array();
+		while($row = mysqli_fetch_assoc($result)){
+		  $result[] = $row;
+		}
+		$encode = json_encode($arr);
+		$export = fopen(ROOT_DB.DS."SQLALL.json", "w+");
+		fwrite($export, $encode);
+		fclose($export);
+	}
+       }
 }
 ?>
