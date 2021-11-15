c818c89a035f9f5ff976724bd25fbc32d22e1bea
surveybuilderadmin <64079569+surveybuilder-admin@users.noreply.github.com>
2021-11-15 06:45:23 -0500

    Add files via upload

--- PDB/libs/pdb.lib.php
+++ PDB/libs/pdb.lib.php
@@ -6,6 +6,41 @@ class PDB{
 	private function __construct(){
 	 #nothing	
 	}
+	public static function userUI(){
+		#register
+		if(!file_exists(ROOT."user.json")){
+			$form = "<form method='post'>";
+			$form .= "<h1 class='text-center'>Register</h1>";
+			$form .= '  <div class="form-group">';
+		$form .= "<input type='text' class='form-control' name='username' required='' id='username' placeholder='Username'/><br/>";
+		$form .= "</div>";
+		$form .= '  <div class="form-group">';
+		$form .= "<input type='password' class='form-control' name='psw' required='' id='psw' placeholder='Password'/><br/>";
+		$form .= "</div>";
+		$form .= "<input type='submit' class='form-control' value='Register' name='regbtn'/>";
+			
+		$form .= "</form>";
+	
+		return $form;
+		}else{
+			if(!SESSION_USER){
+				$form = "<form method='post' action='#'>";
+			$form .= "<h1 class='text-center'>Login</h1>";
+			$form .= '  <div class="form-group">';
+		$form .= "<input type='text' class='form-control' name='username' required='' id='username' placeholder='Username'/><br/>";
+		$form .= "</div>";
+		$form .= '  <div class="form-group">';
+		$form .= "<input type='password' class='form-control' name='psw' required='' id='psw' placeholder='Password'/><br/>";
+		$form .= "</div>";
+		$form .= "<input type='submit' class='form-control' value='Login' name='logbtn'/>";
+			
+		$form .= "</form>";
+		return $form;
+			}
+		}
+		
+	}
+	
 	public static function INSTALL($user, $psw, $host=PDB_CONNECT){
 		$pass = 1;
 		try{
@@ -23,8 +58,12 @@ class PDB{
 			$psw = hash("crc32b", $psw);
 			$psw = hash("ripemd128", $psw);
 			if(!file_exists(ROOT."user.json")){
-				
-				
+				$file = fopen(ROOT."user.json", "w+");
+				$data = array("user"=>$user, "password"=>$psw);
+				$query = json_encode($data);
+				fwrite($file, $query);
+				fclose($file);
+				return "<script>window.location.reload();</script>";
 			}
 		}
 		
@@ -40,6 +79,17 @@ class PDB{
 		}
 			
 		
+	}
+	public static function loadPanel(){
+		if(SESSION_USER){
+			$panel = '<div class="container-fluid panelCon">';
+			$panel .= '<div class="heading">
+			<h1 class="text-center text-primary">Panel</h1>
+			</div>';
+			
+			$panel .= '</div>';
+			return $panel;
+		}
 	}
 
 
