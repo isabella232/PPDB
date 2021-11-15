70665f544f8f0c33818d8e6c6fe550f1b07979dc
surveybuilderadmin <64079569+surveybuilder-admin@users.noreply.github.com>
2021-11-15 13:34:40 -0500

    Update init.php

--- PPDB/libs/bin/init.php
+++ PPDB/libs/bin/init.php
@@ -13,28 +13,28 @@ const LIBRARY_LICENCE = "Apache-2.0 License";
 			# cache LIBRARY values
 		try{
 		if(LIBRARY_NAME !== "PPDB"){
-			throw new PDBErr(LIBRARY_NAME);
+			throw new PPDBErr(LIBRARY_NAME);
 		}
 	}catch(PPDBErr $e){
 			  echo $e->CHECKLIBSNAME();
 	}
 		try{
 		if(LIBRARY_VERSION !== "0.0.1"){
-			throw new PDBErr(LIBRARY_VERSION);
+			throw new PPDBErr(LIBRARY_VERSION);
 		}
 	}catch(PPDBErr $e){
 			  echo $e->CHECKLIBSVERSION();
 	}
 		try{
 		if(LIBRARY_API[0] !== 1){
-			throw new PDBErr(LIBRARY_API[0]);
+			throw new PPDBErr(LIBRARY_API[0]);
 		}
 	}catch(PPDBErr $e){
 			  echo $e->CHECKLIBSAPI();
 	}
 		try{
 		if(gettype(LIBRARY_SSL_SUPPORT) !== "boolean"){
-			throw new PDBErr(LIBRARY_SSL_SUPPORT);
+			throw new PPDBErr(LIBRARY_SSL_SUPPORT);
 		}
 	}catch(PPDBErr $e){
 			  echo $e->CHECKLIBSSL();
@@ -42,7 +42,7 @@ const LIBRARY_LICENCE = "Apache-2.0 License";
 	
 		try{
 		if(LIBRARY_LICENCE !== "Apache-2.0 License"){
-			throw new PDBErr(LIBRARY_LICENCE);
+			throw new PPDBErr(LIBRARY_LICENCE);
 		}
 	}catch(PPDBErr $e){
 			  echo $e->CHECKLIBSLICENCE();
