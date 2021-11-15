351f8eb6861d79f065276ee1c4993df16df84343
surveybuilderadmin <64079569+surveybuilder-admin@users.noreply.github.com>
2021-11-15 13:26:35 -0500

    Update defined.php

--- PPDB/defined.php
+++ PPDB/defined.php
@@ -2,8 +2,8 @@
 define("DS", "\\", false);
 define("ROOT", dirname(__FILE__).DS, false);
 define("ROOT_DB", dirname(__FILE__).DS."libs".DS."db", false);
-define("PDB_CONNECT", $_SERVER['SERVER_NAME'] . ":" . $_SERVER['SERVER_PORT'], false);
-define("PDB_SERVER_NAME", $_SERVER['SERVER_NAME'], false);
-define("PDB_SERVER_PORT", $_SERVER['SERVER_PORT'], false);
+define("PPDB_CONNECT", $_SERVER['SERVER_NAME'] . ":" . $_SERVER['SERVER_PORT'], false);
+define("PPDB_SERVER_NAME", $_SERVER['SERVER_NAME'], false);
+define("PPDB_SERVER_PORT", $_SERVER['SERVER_PORT'], false);
 define("SESSION_USER", $_SESSION['username'], false);
 ?>
