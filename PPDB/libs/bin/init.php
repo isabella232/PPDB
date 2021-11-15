1c03f2645e1e0ab2c60ed3eb54841e875667ed16
surveybuilderadmin <64079569+surveybuilder-admin@users.noreply.github.com>
2021-11-15 13:28:54 -0500

    Update init.php

--- PPDB/libs/bin/init.php
+++ PPDB/libs/bin/init.php
@@ -1,7 +1,7 @@
 <?php
 
 # package name
-const LIBRARY_NAME = "PDB";
+const LIBRARY_NAME = "PPDB";
 const LIBRARY_VERSION = "0.0.1";
 const LIBRARY_API = [1];
 const LIBRARY_SSL_SUPPORT = true;
@@ -12,31 +12,31 @@ const LIBRARY_LICENCE = "Apache-2.0 License";
    function RenderLibrary(){
 			# cache LIBRARY values
 		try{
-		if(LIBRARY_NAME !== "PDB"){
+		if(LIBRARY_NAME !== "PPDB"){
 			throw new PDBErr(LIBRARY_NAME);
 		}
-	}catch(PDBErr $e){
+	}catch(PPDBErr $e){
 			  echo $e->CHECKLIBSNAME();
 	}
 		try{
 		if(LIBRARY_VERSION !== "0.0.1"){
 			throw new PDBErr(LIBRARY_VERSION);
 		}
-	}catch(PDBErr $e){
+	}catch(PPDBErr $e){
 			  echo $e->CHECKLIBSVERSION();
 	}
 		try{
 		if(LIBRARY_API[0] !== 1){
 			throw new PDBErr(LIBRARY_API[0]);
 		}
-	}catch(PDBErr $e){
+	}catch(PPDBErr $e){
 			  echo $e->CHECKLIBSAPI();
 	}
 		try{
 		if(gettype(LIBRARY_SSL_SUPPORT) !== "boolean"){
 			throw new PDBErr(LIBRARY_SSL_SUPPORT);
 		}
-	}catch(PDBErr $e){
+	}catch(PPDBErr $e){
 			  echo $e->CHECKLIBSSL();
 	}
 	
@@ -44,7 +44,7 @@ const LIBRARY_LICENCE = "Apache-2.0 License";
 		if(LIBRARY_LICENCE !== "Apache-2.0 License"){
 			throw new PDBErr(LIBRARY_LICENCE);
 		}
-	}catch(PDBErr $e){
+	}catch(PPDBErr $e){
 			  echo $e->CHECKLIBSLICENCE();
 	}
 	
