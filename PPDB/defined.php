3ae0aeb4ae22e98eeae4cd85722acacc1a11ec0e
surveybuilderadmin <64079569+surveybuilder-admin@users.noreply.github.com>
2021-11-23 07:56:37 -0500

    Updated FORWARD defines

--- PPDB/defined.php
+++ PPDB/defined.php
@@ -3,6 +3,8 @@ define("DS", "\\", false);
 define("DS_FOWARD", "/", false);
 define("ROOT", dirname(__FILE__).DS, false);
 define("ROOT_DB", dirname(__FILE__).DS."db".DS, false);
+define("ROOT_FOWARD", dirname(__FILE__).DS_FOWARD, false);
+define("ROOT_DB_FOWARD", dirname(__FILE__).DS_FOWARD."db".DS_FOWARD, false);
 define("PPDB_CONNECT", $_SERVER['SERVER_NAME'] . ":" . $_SERVER['SERVER_PORT'], false);
 define("PPDB_SERVER_NAME", $_SERVER['SERVER_NAME'], false);
 define("PPDB_SERVER_PORT", $_SERVER['SERVER_PORT'], false);
