5b2f39204080ce5db0c7420931cb5575465e9c68
surveybuilderadmin <64079569+surveybuilder-admin@users.noreply.github.com>
2021-11-23 07:53:52 -0500

    Updated DS_FOWARD
    Unlike DS where it does a "\", DS_FOWARD uses "/"

--- PPDB/defined.php
+++ PPDB/defined.php
@@ -1,5 +1,6 @@
 <?php
 define("DS", "\\", false);
+define("DS_FOWARD", "/", false);
 define("ROOT", dirname(__FILE__).DS, false);
 define("ROOT_DB", dirname(__FILE__).DS."db".DS, false);
 define("PPDB_CONNECT", $_SERVER['SERVER_NAME'] . ":" . $_SERVER['SERVER_PORT'], false);
