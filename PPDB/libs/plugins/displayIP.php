013ea5543f502654296e75bcc11b392329d4d55e
surveybuilder-admin <64079569+surveybuilder-admin@users.noreply.github.com>
2021-12-14 09:00:07 -0500

    Update displayIP.php

--- PPDB/libs/plugins/displayIP.php
+++ PPDB/libs/plugins/displayIP.php
@@ -1,5 +1,10 @@
 <?php
+class displayIP{
+    private function __contruct(){
+    # Nothing
+    }
 if(!@require("handler/PluginHandler.php")) echo "Plugin Handler not found";
+define("USER_IP", ['ip'=>'', 'city'=>'', 'region'=>'', 'country'=>'', 'loc'=>'', 'org'=>'', 'postal'=>'', 'timezone'=>'']);
 if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
     $ip = $_SERVER['HTTP_CLIENT_IP'];
 } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
@@ -7,6 +12,24 @@ if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
 } else {
     $ip = $_SERVER['REMOTE_ADDR'];
 }
-$decode = json_decode("http://ipinfo.io/{$ip}/json");
+$d = json_decode("http://ipinfo.io/{$ip}/json");
+USER_IP['ip'] = $d->ip;
+USER_IP['city'] = $d->city;
+USER_IP['region'] = $d->region;
+USER_IP['country'] = $d->country;
+USER_IP['loc'] = $d->loc;
+USER_IP['org'] = $d->org;
+USER_IP['postal'] = $d->postal;
+USER_IP['timezone'] = $d->timezone;
+    
+function returnData($data=[]){
+
+}    
+    
+    
+}
+
+
+
 
 ?>
