78a05e42e8c50a7cbe23c81cbb9289f70458d172
surveybuilder-admin <64079569+surveybuilder-admin@users.noreply.github.com>
2021-12-14 09:49:42 -0500

    Update displayIP.php

--- PPDB/libs/plugins/displayIP.php
+++ PPDB/libs/plugins/displayIP.php
@@ -1,9 +1,7 @@
 <?php
 class displayIP{
-    private function __contruct(){
-    # Nothing
-    }
-if(!@require("handler/PluginHandler.php")) echo "Plugin Handler not found";
+ public function __contruct(){
+    if(!@require("handler/PluginHandler.php")) echo "Plugin Handler not found";
 define("USER_IP", ['ip'=>'', 'city'=>'', 'region'=>'', 'country'=>'', 'loc'=>'', 'org'=>'', 'postal'=>'', 'timezone'=>'']);
 if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
     $ip = $_SERVER['HTTP_CLIENT_IP'];
@@ -21,9 +19,22 @@ USER_IP['loc'] = $d->loc;
 USER_IP['org'] = $d->org;
 USER_IP['postal'] = $d->postal;
 USER_IP['timezone'] = $d->timezone;
-    
-function returnData($data=[]){
+   }
 
+    
+public static function returnData($data=[]){
+    try{
+        if(!PPDBLogic::isArray($data)){
+        throw new PPDBErr($data);
+     }
+    }catch(PPDBErr $e){
+        $e->isNotArray();
+    }
+    
+    foreach($data as $d){
+    echo USER_IP[$d];
+    }
+    
 }    
     
     
